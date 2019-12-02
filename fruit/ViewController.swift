6930795281df678fc90b6966c97a22bd439f9bfa
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-12-02 15:17:20 +0900

    Initial Commit

---
+++ fruit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fruit
+//
+//  Created by D7703_16 on 2019. 12. 2..
+//  Copyright © 2019년 D7703_16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
